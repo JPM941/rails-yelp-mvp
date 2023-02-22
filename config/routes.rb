3d9897f430f7870dd8cea556b2377a007183a218
Julien Martin <julien.martin94@icloud.com>
2023-02-22 15:06:38 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'reviews/new'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,20 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+mcdo = {name: "mcdo", address: "je sais pas ou", phone_number: 0635622233, category: "chinese"}
+pizza =  {name: "Pizza", address: "16 rue de je ne sais pas", phone_number: 0635622534, category: "chinese"}
+kfc =  {name: "Kfc", address: "458 avenue de je ne sais pas", phone_number: 0635622335, category: "chinese"}
+burger_king =  {name: "burger king", address: "458 avenue de la rue", phone_number: 0635622435, category: "chinese"}
+azerty =  {name: "azerty", address: "sur mon clavier", phone_number: 0635622133, category: "chinese"}
+
+[mcdo, pizza, kfc, burger_king, azerty].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
